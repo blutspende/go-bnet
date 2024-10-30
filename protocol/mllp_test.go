14aca83175842091f66405fb9a17e3879684aa50
balazsmate <mate.balazs@minero.hu>
2024-10-30 09:58:21 +0100

    Revert previous change, add CR to end of MLLP messages as per specification.

--- protocol/mllp_test.go
+++ protocol/mllp_test.go
@@ -1,7 +1,6 @@
 package protocol
 
 import (
-	"github.com/blutspende/go-bnet/protocol/utilities"
 	"github.com/stretchr/testify/assert"
 	"net"
 	"testing"
@@ -13,37 +12,19 @@ func TestMLLPProtocolReceive(t *testing.T) {
 	go func() {
 		instrument.Write([]byte("\x0BHello\x0D"))
 		instrument.Write([]byte("IsItMeYoureLookingFor\x0D"))
-		instrument.Write([]byte("Done\x0D\x1C"))
-	}()
-	transmission, err := instance.Receive(host)
-	assert.Nil(t, err)
-	// without start and end byte
-	assert.Equal(t, "Hello\x0DIsItMeYoureLookingFor\x0DDone\x0D", string(transmission))
-}
-
-func TestMLLPProtocolReceiveMultipleStartEndBytes(t *testing.T) {
-	host, instrument := net.Pipe()
-	settings := DefaultMLLPProtocolSettings().SetStartBytes([]byte{utilities.VT, utilities.CR}).SetEndBytes([]byte{utilities.FS, utilities.CR})
-	instance := Logger(MLLP(settings))
-	go func() {
-		instrument.Write([]byte("\x0B\x0DHello\x0D"))
-		instrument.Write([]byte("IsItMeYoureLookingFor\x0D"))
 		instrument.Write([]byte("Done\x1C\x0D"))
-
 	}()
 	transmission, err := instance.Receive(host)
 	assert.Nil(t, err)
-	// without start and end byte
 	assert.Equal(t, "Hello\x0DIsItMeYoureLookingFor\x0DDone", string(transmission))
 }
 
-func TestMLLPProtocolReceiveMultipleStartEndBytesWithExtraPrecedingCharacters(t *testing.T) {
+func TestMLLPProtocolReceiveWithExtraPrecedingCharacters(t *testing.T) {
 	host, instrument := net.Pipe()
-	settings := DefaultMLLPProtocolSettings().SetStartBytes([]byte{utilities.VT, utilities.CR}).SetEndBytes([]byte{utilities.FS, utilities.CR})
-	instance := Logger(MLLP(settings))
+	instance := Logger(MLLP())
 	go func() {
 		instrument.Write([]byte("IShouldBeIgnored"))
-		instrument.Write([]byte("\x0B\x0DHello\x0D"))
+		instrument.Write([]byte("\x0BHello\x0D"))
 		instrument.Write([]byte("IsItMeYoureLookingFor\x0D"))
 		instrument.Write([]byte("Done\x1C\x0D"))
 
@@ -52,4 +33,25 @@ func TestMLLPProtocolReceiveMultipleStartEndBytesWithExtraPrecedingCharacters(t 
 	assert.Nil(t, err)
 	// without start and end byte
 	assert.Equal(t, "Hello\x0DIsItMeYoureLookingFor\x0DDone", string(transmission))
+}
+
+func TestMLLPProtocolSend(t *testing.T) {
+	host, instrument := net.Pipe()
+	instance := Logger(MLLP())
+	receivedBytesChan := make(chan []byte)
+	go func() {
+		buffer := make([]byte, 4096)
+		n, err := instrument.Read(buffer)
+		assert.Nil(t, err)
+		receivedBytesChan <- buffer[:n]
+	}()
+
+	instance.Send(host, [][]byte{
+		[]byte("First frame"),
+		[]byte("Second frame"),
+		[]byte("Last frame"),
+	})
+
+	receivedBytes := <-receivedBytesChan
+	assert.Equal(t, []byte("\x0BFirst frame\x0DSecond frame\x0DLast frame\x0D\x1C\x0D"), receivedBytes)
 }
